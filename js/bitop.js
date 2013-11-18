b078918d0cd774be5e0ee40aacb77d6cc18704b8
Saito Yutaka <melody.pea@gmail.com>
2013-11-19 08:44:46 +0900

    add dec2hex

--- js/bitop.js
+++ js/bitop.js
@@ -1,23 +1,30 @@
-// function dec2hex(d) {
-//     switch(d){
-//     case '0' return '0';
-//     case '1' return '1';
-//     case '2' return '2';
-//     case '3' return '3';
-//     case '4' return '4';
-//     case '5' return '5';
-//     case '6' return '6';
-//     case '7' return '7';
-//     case '8' return '8';
-//     case '9' return '9';
-//     case '10' return 'a';
-//     case '11' return 'b';
-//     case '12' return 'c';
-//     case '13' return 'd';
-//     case '14' return 'e';
-//     case '15' return 'f';
-//     default: return '';
-// }
+function d2h(d){
+    switch(d){
+    case '0' return '0';
+    case '1' return '1';
+    case '2' return '2';
+    case '3' return '3';
+    case '4' return '4';
+    case '5' return '5';
+    case '6' return '6';
+    case '7' return '7';
+    case '8' return '8';
+    case '9' return '9';
+    case '10' return 'a';
+    case '11' return 'b';
+    case '12' return 'c';
+    case '13' return 'd';
+    case '14' return 'e';
+    case '15' return 'f';
+    default: return '0';
+    }
+}
+
+function dec2hex(d) {
+    var hex = new String();
+    hex += d2h((d - 15) / 16);
+    hex += d2h(d - 240);
+}
 
 function hex2bit(h) {
     var b = '';
